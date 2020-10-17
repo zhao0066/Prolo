3c7d21b903d7ef0745201c1863169fae7dfb93fc
Thomas <curt0053@algonquinlive.com>
2020-10-17 13:58:52 -0400

    Merged code so you can now get to the settings screen from the main screen by clicking on the gear icon

---
+++ app/src/main/java/com/example/prolo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.prolo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        Toolbar tbar = findViewById(R.id.toolbar);
+        setSupportActionBar(tbar);
+        getSupportActionBar().setTitle(null);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        tbar.setNavigationIcon(R.drawable.logo);
+        tbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Do whatever you want to do here
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.pencilItem:
+                break;
+
+            case R.id.gearItem:
+                Intent goToSettings = new Intent(MainActivity.this, com.example.prolo.SettingsActivity.class);
+                startActivity(goToSettings);
+                break;
+
+            case R.id.profileItem:
+                break;
+        }
+
+        return true;
+    }
+}
