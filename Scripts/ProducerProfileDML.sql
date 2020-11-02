1558bd388d2b933205e8f0b2fb448dc93704936f
Angela Zhao <zhao0066@algonquinlive.com>
2020-11-02 02:22:07 -0500

    Add Database DDL and DML script files

---
+++ Scripts/ProducerProfileDML.sql
@@ -0,0 +1,22 @@
+DELETE FROM PRODUCER_PROFILE;
+
+INSERT INTO PRODUCER_PROFILE (PRODUCER_ID, COMPANY_NAME, CONTACT_NAME, STREET, CITY, PROV_STATE, COUNTRY, PCODE, PHONE, CONTACT_EMAIL, WEBSITE, GMAP_PLUS_CODE)
+VALUES (10001, "Wilhaven Ridge Alpacas", "Hans & Colleen Brouwer", "1620 Wilhaven Drive", "Cumberland", "Ontario", "Canada", "", "613-833-3227", "info@wilhavenridgealpacas.ca", "http://www.wilhavenridgealpacas.ca/", "FHW5+7F Cumberland, Ottawa, ON");
+
+INSERT INTO PRODUCER_PROFILE (PRODUCER_ID, COMPANY_NAME, CONTACT_NAME, STREET, CITY, PROV_STATE, COUNTRY, PCODE, PHONE, CONTACT_EMAIL, WEBSITE, GMAP_PLUS_CODE)
+VALUES (10002, "La ferme d'Orléans", "Paul et Françoise Henri", "1399 St-Joseph", "Ottawa", "Ontario", "Canada", "K1C 7K9", "613-830-1303", "", "www.fermedorleans.com", "FC5J+2X Gloucester, Ottawa, ON");
+
+INSERT INTO PRODUCER_PROFILE (PRODUCER_ID, COMPANY_NAME, CONTACT_NAME, STREET, CITY, PROV_STATE, COUNTRY, PCODE, PHONE, CONTACT_EMAIL, WEBSITE, GMAP_PLUS_CODE)
+VALUES (10003, "Ferme Proulx et Érablière", "", "1865 O'Toole", "Cumberland", "Ontario", "Canada", "K4C 1N2", "613-833-2417", "info@proulxfarm.com", "www.proulxfarm.com", "FHMH+VM Cumberland, Ottawa, ON");
+
+INSERT INTO PRODUCER_PROFILE (PRODUCER_ID, COMPANY_NAME, CONTACT_NAME, STREET, CITY, PROV_STATE, COUNTRY, PCODE, PHONE, CONTACT_EMAIL, WEBSITE, GMAP_PLUS_CODE)
+VALUES (10004, "Potager Eardley Co.", "", "398 Chemin Eardley", "Gatineau", "Québec", "Canada", "J9J 2Y9", "", "", "www.eardleyco.com", "C4F5+4X Gatineau, Quebec");
+
+INSERT INTO PRODUCER_PROFILE (PRODUCER_ID, COMPANY_NAME, CONTACT_NAME, STREET, CITY, PROV_STATE, COUNTRY, PCODE, PHONE, CONTACT_EMAIL, WEBSITE, GMAP_PLUS_CODE)
+VALUES (10005, "Ferme de Viltain", "", "78350, Chemin de Viltain", "Jouy-en-Josas", "", "France", "", "", "", "www.viltain.fr", "Q535+PC Jouy-en-Josas, France");
+
+INSERT INTO PRODUCER_PROFILE (PRODUCER_ID, COMPANY_NAME, CONTACT_NAME, STREET, CITY, PROV_STATE, COUNTRY, PCODE, PHONE, CONTACT_EMAIL, WEBSITE, GMAP_PLUS_CODE)
+VALUES (10006, "Hacienda La Bonita", "", "29100 Coin", "Malaga", "", "Spain", "", "", "", "www.labonitahacienda.com", "M6QW+8X Coín, Spain");
+
+INSERT INTO PRODUCER_PROFILE (PRODUCER_ID, COMPANY_NAME, CONTACT_NAME, STREET, CITY, PROV_STATE, COUNTRY, PCODE, PHONE, CONTACT_EMAIL, WEBSITE, GMAP_PLUS_CODE)
+VALUES (10007, "Suzie's yard", "", "SS 321 SUD km 12, 30, 53040 Cetona", "Cetona", "Siena", "Italy", "", "+39 348 854 3665", "", "www.suziesyard.com", "WWPF+WQ Cetona, Province of Siena, Italy");
